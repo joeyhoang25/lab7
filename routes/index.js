89f8b53e4d8eeeded37e4821945cbef882338a40
Joey Hoang <j4hoang@ucsd.edu>
2021-02-18 13:21:53 -0800

    added to handlebars and js files

--- routes/index.js
+++ routes/index.js
@@ -5,5 +5,16 @@ var projects = require('../projects.json');
  */
 
 exports.view = function(request, response){
+  	projects["viewAlt"] = false;
   	response.render('index', projects);
+ 	//data['viewAlt'] = false;
+ 	
 };
+
+exports.viewAlt = function(request, response) {
+	response.render('index', projects);
+	projects["viewAlt"] = true;
+	
+
+}
+
